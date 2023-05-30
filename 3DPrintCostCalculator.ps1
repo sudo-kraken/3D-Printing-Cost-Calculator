2514dc3974a1c5190122bdd547422cac47f0b3bb
Joe Harrison <53116754+Joeharrison94@users.noreply.github.com>
2023-05-30 08:56:36 +0100

    Update 3DPrintCostCalculator.ps1

--- 3DPrintCostCalculator.ps1
+++ 3DPrintCostCalculator.ps1
@@ -8,18 +8,24 @@ $printTime = [float](Read-Host "Please enter the Print Time (hr)")
 $electricityCost = [float](Read-Host "Please enter the Local Electricity Cost (£/kWh)")
 $printerPower = [float](Read-Host "Please enter the 3D Printer Power Consumption (W)")
 $profitMargin = [float](Read-Host "Please enter the Profit Margin (%)")
+$shippingCost = [float](Read-Host "Please enter the Shipping Cost (£)")
 
 # Calculate costs
 $materialCost = ($materialPrice * ($materialConsumed / 1000))
 $laborCost = $laborRate * $laborTime
 $equipmentCost = $printerCharge * $printTime
-$electricityCost = (($printerPower / 1000) * $printTime * $electricityCost)
+$electricityCost = ($printerPower / 1000) * $electricityCost * $printTime
 $totalCost = $materialCost + $laborCost + $equipmentCost + $electricityCost
 
 # Calculate profit and total charge
 $profit = ($profitMargin / 100) * $totalCost
-$totalCharge = $totalCost + $profit
+$totalCharge = $totalCost + $profit + $shippingCost
 
-# Output total charge, rounded to 2 decimal places
+# Round total charge to 2 decimal places
 $totalCharge = [math]::Round($totalCharge, 2)
-Write-Output "Total 3D Printing Charge (£) = $totalCharge"
+
+# Choose currency symbol
+$currencySymbol = Read-Host 'Please enter the currency symbol (£, $, €)'
+
+# Output total charge with chosen currency symbol
+Write-Output "Total 3D Printing Charge ($currencySymbol) = $totalCharge"
